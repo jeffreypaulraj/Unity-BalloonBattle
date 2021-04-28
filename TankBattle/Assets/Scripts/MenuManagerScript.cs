2c0265e6e6c3f26fea9cd4982beb4b416440cb33
Jeffrey Paulraj <jeffreypaulraj@users.noreply.github.com>
2021-04-28 10:51:05 -0400

    added menu

---
+++ TankBattle/Assets/Scripts/MenuManagerScript.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+public class MenuManagerScript : MonoBehaviourPunCallbacks
+{
+    public Button joinCreateButton;
+    public Button startButton;
+    public InputField codeInputField;
+    public InputField nameField;
+    public Text playerCountText;
+    public Text playerList;
+    public static MenuManagerScript instance;
+    bool gameCreated;
+    int playerCount;
+
+    void Awake() {
+        if (instance != null && instance != this) {
+            gameObject.SetActive(false);
+        }
+        else{
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    void Start() {
+        PhotonNetwork.ConnectUsingSettings();
+        joinCreateButton.onClick.AddListener(JoinCreateRoom);
+        startButton.onClick.AddListener(StartGameCheck);
+        gameCreated = false;
+        playerCount = 0;
+    }
+
+    public override void OnCreatedRoom(){
+        Debug.Log("Created room: " + PhotonNetwork.CurrentRoom.Name);
+    }
+
+    public void JoinOrCreateRoom(string roomName) {
+        PhotonNetwork.JoinOrCreateRoom(roomName, null, null, null);
+        PhotonNetwork.LocalPlayer.NickName = nameField.text;
+        gameCreated = true;
+    }
+
+    public void ChangeScene() {
+        PhotonNetwork.LoadLevel("Game");
+    }
+
+    public void StartGameCheck()
+    {
+        if (gameCreated) {
+            ChangeScene();
+        }
+    }
+
+    void Update(){
+        if (gameCreated){
+            playerCount = PhotonNetwork.CountOfPlayers;
+            playerCountText.text = "Number of Players in Room: " + playerCount;
+            string playerListString = "";
+            foreach (var player in PhotonNetwork.PlayerList) {
+                playerListString += player.NickName + "\n";
+            }
+            playerList.text = playerListString;
+
+        }
+    }
+
+    void JoinCreateRoom(){
+        string code = codeInputField.text;
+        JoinOrCreateRoom(code);
+    }
+}
---
+++ TankBattle/Assets/Scripts/NetworkManagerScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class NetworkManagerScript : MonoBehaviourPunCallbacks
+{    
+    
+
+    void Start()
+    {
+    }
+
+    public override void OnConnectedToMaster(){
+        Debug.Log("Connected to Master Server");
+    }
+
+    public override void OnCreatedRoom(){
+        Debug.Log("Created room: " + PhotonNetwork.CurrentRoom.Name);
+    }
+
+    public void JoinOrCreateRoom(string roomName)
+    {
+        PhotonNetwork.JoinOrCreateRoom(roomName, null, null, null);
+    }
+    public void ChangeScene(string sceneName){
+        PhotonNetwork.LoadLevel(sceneName);
+    }
+
+    public void Update()
+    {
+        
+    }
+}
